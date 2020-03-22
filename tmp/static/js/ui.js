0915bb1212a7084cce36ea694f0303560649f118
junfrenkixp <penin13579@gmail.com>
2020-03-23 01:28:31 +0300

    fix ie

--- tmp/static/js/ui.js
+++ tmp/static/js/ui.js
@@ -29,7 +29,7 @@
         </div>
       </button>`
     });
-  })
+  });
 
   document.documentElement.addEventListener(
     'touchstart',
@@ -39,33 +39,31 @@
       }
     },
     false,
-  )
+  );
 
-    const nav = document.querySelector('.header__nav');
-    nav.addEventListener('click', event => {
-      let target = event.target;
-      if (target.closest('.header__nav-mob__toggle')) {
-        nav.classList.toggle('active');
-      }
-    })
+  const nav = document.querySelector('.header__nav');
+  nav.addEventListener('click', event => {
+    let target = event.target;
+    if (target.closest('.header__nav-mob__toggle')) {
+      nav.classList.toggle('active');
+    }
+  });
 
 
 
-    let allBtns = document.querySelectorAll('.js-accord-btn');
-
-    allBtns.forEach( btn => {
-      btn.addEventListener('click', event => {
-        let items = document.querySelectorAll('.accordeon__item');
-        for (let item of items) {
-          if (item.classList.contains('active')) {
-              item.classList.remove('active');
-          }
+  let allBtns = document.querySelectorAll('.js-accord-btn');
+  
+  allBtns.forEach( btn => {
+    btn.addEventListener('click', event => {
+      let items = document.querySelectorAll('.accordeon__item');
+      for (let item of items) {
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
         }
-      let item = event.target.closest('.accordeon__item');
-      item.classList.add('active');
-      })
+      }
+    let item = event.target.closest('.accordeon__item');
+    item.classList.add('active');
     })
-
-
+  });
 
 })()
