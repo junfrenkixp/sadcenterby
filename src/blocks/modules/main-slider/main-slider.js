805d6e6eb80d66c3bc3abd0db0cd8121c4aa0e48
Artsiom Zavaley <penin13579@gmail.com>
2021-05-09 17:02:12 +0300

    fix main slider

--- src/blocks/modules/main-slider/main-slider.js
+++ src/blocks/modules/main-slider/main-slider.js
@@ -1,13 +1,15 @@
 import $ from "jquery";
-import 'slick-carousel/slick/slick';
-'use strict';
+import "slick-carousel/slick/slick";
+"use strict";
 
-window.addEventListener('DOMContentLoaded', function() {
-    $('.main-slider').slick({
-        dots: false,
+window.addEventListener("DOMContentLoaded", function() {
+    $(".main-slider").slick({
+        dots: true,
         fade: true,
         infinite: true,
         arrows: false,
-        swipe: false
+        swipe: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
     });
-})
+});
